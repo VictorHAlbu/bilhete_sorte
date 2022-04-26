d1f846aaf539a05c1ebd6e3f1ae27152bfa4e03e
Victor Hugo Albuquerque <vh12albuuerque@gmail.com>
2022-04-25 22:36:35 -0300

    bilhetes e casos de uso

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_25_010818) do
+ActiveRecord::Schema.define(version: 2022_04_25_011548) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 2022_04_25_010818) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "tickets", force: :cascade do |t|
+    t.string "name"
+    t.float "valor"
+    t.text "numbers_ticket", default: [], array: true
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
