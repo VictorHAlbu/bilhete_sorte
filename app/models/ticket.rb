aa61ddc0cf4bd92c81885224e89a08ac6291ba33
VictorHAlbu <vh12albuquerque@gmail.com>
2022-05-03 21:49:03 -0300

    garantindo bilhete úniico

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,7 +1,9 @@
 class Ticket < ApplicationRecord
 
   before_save :generate_numbers_ticket
-  
+  validates_uniqueness_of :numbers_ticket
+  validate :posts_count_within_limit, on: :create
+
   attr_accessor :numbers
 
   # Todas milhar
@@ -12,22 +14,29 @@ class Ticket < ApplicationRecord
   def total_numbers_in_single_array
     total_of_thousands.flatten
   end
-
-  def check_milhar_exist(numbers)
-   value = numbers.find_all { |pos| numbers.count(pos) > 1 }    
-  end
-
   
   private
-
+  
   def generate_numbers_ticket
-
     numbers = (0...4).collect { sprintf('%04d', rand(10000)) }.uniq
+    check_milhar_exist(numbers)
     debugger
-    self.numbers_ticket = numbers
+    self.numbers_ticket = numbers.to_a
+  end
+  
+  
+  def check_milhar_exist(array)
+    if array.all? { |e| total_numbers_in_single_array.include?(e) }
+      return errors[:numbers_ticket] << "Milhar já este!!! Por favor, crie outro Bilhete." 
+    end
   end
 
- 
+  def posts_count_within_limit
+    if total_numbers_in_single_array.count >= 10000
+      errors.add(:base, 'Limite total de bilhetes vendidos.')
+    end
+  end
+
 
   
 end
