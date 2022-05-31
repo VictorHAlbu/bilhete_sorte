41c1e5094b89b9010ef2b9215f756285279f579a
VictorHAlbu <vh12albuquerque@gmail.com>
2022-05-30 21:07:42 -0300

    tickets check_milhar_exist

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -11,24 +11,26 @@ class Ticket < ApplicationRecord
     return totalnumbers = Ticket.all.map{|ticket| ticket.numbers_ticket }
   end
 
+  # total de milhar em único array
   def total_numbers_in_single_array
     total_of_thousands.flatten
+  end
+  
+  def check_milhar_exist(array)
+    if array.all? { |e| total_numbers_in_single_array.include?(e) }
+      return errors[:numbers_ticket] << "Milhar já este!!! Por favor, crie outro Bilhete." 
   end
   
   private
   
   def generate_numbers_ticket
     numbers = (0...4).collect { sprintf('%04d', rand(10000)) }.uniq
+    total_of_thousands.append(numbers)
     check_milhar_exist(numbers)
-    debugger
-    self.numbers_ticket = numbers.to_a
+    self.numbers_ticket = numbers
   end
   
   
-  def check_milhar_exist(array)
-    if array.all? { |e| total_numbers_in_single_array.include?(e) }
-      return errors[:numbers_ticket] << "Milhar já este!!! Por favor, crie outro Bilhete." 
-    end
   end
 
   def posts_count_within_limit
