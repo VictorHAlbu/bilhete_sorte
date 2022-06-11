8b6bc39785832477351204ec24d3e31197d0751e
Victor Hugo Albuquerque <vh12albuuerque@gmail.com>
2022-06-11 16:10:49 -0300

    relation current_user and ticket

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,6 +3,7 @@ class Ticket < ApplicationRecord
   before_save :generate_numbers_ticket
   validates_uniqueness_of :numbers_ticket
   validate :posts_count_within_limit, on: :create
+  belongs_to :user
 
   attr_accessor :numbers
 
@@ -19,19 +20,18 @@ class Ticket < ApplicationRecord
   def check_milhar_exist(array)
     if array.all? { |e| total_numbers_in_single_array.include?(e) }
       return errors[:numbers_ticket] << "Milhar já este!!! Por favor, crie outro Bilhete." 
+    end
   end
-  
+
   private
   
   def generate_numbers_ticket
     numbers = (0...4).collect { sprintf('%04d', rand(10000)) }.uniq
-    total_of_thousands.append(numbers)
     check_milhar_exist(numbers)
     self.numbers_ticket = numbers
   end
   
   
-  end
 
   def posts_count_within_limit
     if total_numbers_in_single_array.count >= 10000
